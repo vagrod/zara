d7a9b95a43264ee7f3f160e84d191cb1ae5e8bd5
Vadim Gromov <vagrod@live.ru>
2020-12-26 17:48:59 +0300

    NetCore demo: now this demo showcases multiple persons scenario No changes to Zara engine

--- zara-demo-netcore/Person.cs
+++ zara-demo-netcore/Person.cs
@@ -4,10 +4,9 @@ using ZaraEngine.Player;
 using ZaraEngine.Inventory;
 using ZaraEngine.HealthEngine;
 
-public class GameController : IGameController
+// It's our "GameController"
+public class Person : IPerson
 {
-
-    private DateTime _dateTime;
 
     private PlayerStatus _player;
 
@@ -19,91 +18,60 @@ public class GameController : IGameController
 
     private InventoryController _inventory;
 
-    private TimesOfDay _timeOfDay;
-
-    private System.Random _random;
-
     #region Demo app fields
 
-    private System.Threading.Thread _loop;
+    private readonly System.Random _random;
+    private readonly string _name;
     private float _infoUpdateCounter;
-    private float _dateTimeCounter;
+    private Func<DateTime?> _getDateTimeFunc;
+    private Func<TimesOfDay> _getTimeOfDayFunc;
 
     #endregion 
 
-    public void Initialize()
-    {
+    public Person(string name){
+        _name = name;
         _random = new Random(DateTime.Now.Millisecond);
+    }
+
+    public void Initialize(WeatherDescription weather, Func<DateTime?> getDateTimeFunc, Func<TimesOfDay> getTimeOfDayFunc)
+    {
+        _getDateTimeFunc = getDateTimeFunc;
+        _getTimeOfDayFunc = getTimeOfDayFunc;
 
         /* Zara Initialization code start =======>> */
 
         ZaraEngine.Helpers.InitializeRandomizer((a, b) => (float)(a + ((b-a) * _random.NextDouble())));
 
-        _dateTime = DateTime.Now;
-        _timeOfDay = TimesOfDay.Evening;
         _health = new HealthController(this);
         _body = new BodyStatusController(this);
-        _weather = new WeatherDescription();
+        _weather = weather;
         _player = new PlayerStatus();
         _inventory = new InventoryController(this);
-
         _body.Initialize();
         _health.Initialize();
 
         /* <<======= Zara Initialization code end */
-
-        #region Demo app init
-
-        // Defaults
-        _weather.SetTemperature(27f);
-        _weather.SetWindSpeed(0.1f);
-        _weather.SetRainIntensity(0f);
-
-        // Start the "game loop"
-        _loop = new System.Threading.Thread(LoopThread, 0);
-        _loop.Start();
-
-        #endregion
     }
 
-    private void LoopThread(object state)
-    {
-        var time = DateTime.Now;
-
-        while(true)
-        {
-            Update((float)(DateTime.Now - time).TotalSeconds);
-
-            time = DateTime.Now;
-
-            // Cap the "framerate"
-            System.Threading.Thread.Sleep(33);
-        } 
-    }
-
-    private void Update(float deltaTime)
+    public void Update(float deltaTime)
     {
         /* These two calls are required by Zara */
         
         _body.Check(deltaTime);
         _health.Check(deltaTime);
 
-        #region Demo App: Advancing the Time and displaying Output
+        #region Demo App: displaying Output
 
         _infoUpdateCounter += deltaTime;
-        _dateTimeCounter += deltaTime;
-
-        if (_dateTimeCounter > 0.05f)
-        {
-            _dateTime = _dateTime.AddSeconds(0.5d); // in-game time is 10x the real one
-            _dateTimeCounter = 0f;
-        }
 
         if (_infoUpdateCounter >= 1f)
         {
             _infoUpdateCounter = 0f;
 
-            Console.WriteLine($"Health: body temp. is {_health.Status.BodyTemperature} deg C");
+            Console.WriteLine($"{_name}: body temp. is {_health.Status.BodyTemperature} deg C");
+            Console.WriteLine($"{_name}: warmth level is {_body.WarmthLevelCached}");
+            Console.WriteLine($"{_name}: wetness is {_body.WetnessLevel}");
+            Console.WriteLine();
         }
 
         #endregion
@@ -111,7 +79,7 @@ public class GameController : IGameController
 
     #region IGameController Implementation -- Required by Zara
 
-    public DateTime? WorldTime => _dateTime;
+    public DateTime? WorldTime => _getDateTimeFunc();
 
     public IPlayerStatus Player => _player;
 
@@ -123,7 +91,7 @@ public class GameController : IGameController
 
     public InventoryController Inventory => _inventory;
 
-    public TimesOfDay TimeOfDay => _timeOfDay;
+    public TimesOfDay TimeOfDay => _getTimeOfDayFunc();
 
     #endregion
 
--- zara-demo-netcore/Program.cs
+++ zara-demo-netcore/Program.cs
@@ -4,14 +4,101 @@ namespace ZaraEngine.NetCore.Demo
 {
     class Program
     {
-        private static GameController _gc;
+        // Declare two persons
+        private static Person _person1 = null;
+        private static Person _person2 = null;
+
+        // Trepliev will be in a normal climate
+        private static WeatherDescription _weatherForTrepliev;
+
+        // For Freud we'll set up desert climate
+        private static WeatherDescription _weatherForFreud;
+
+        // Declare our "game loop"
+        private static System.Threading.Thread _loop;
+
+        // Counter for progressing the game time
+        private static float _dateTimeCounter;
+
+        // Our game time itself
+        private static DateTime _dateTime;
+
+        // Our in-game time of day
+        private static TimesOfDay _timeOfDay;
 
         static void Main(string[] args)
         {
-            _gc = new GameController();
-            _gc.Initialize();
+            // Initialize our weather
+            _weatherForTrepliev = new WeatherDescription();
+            _weatherForTrepliev.SetTemperature(27f);
+            _weatherForTrepliev.SetWindSpeed(0.1f);
+            _weatherForTrepliev.SetRainIntensity(0.15f);
+
+            _weatherForFreud = new WeatherDescription();
+            _weatherForFreud.SetTemperature(57f);
+            _weatherForFreud.SetWindSpeed(5.0f);
+            _weatherForFreud.SetRainIntensity(0f);
+
+            // Initialize our in-game time
+            _dateTime = DateTime.Now;
+            _timeOfDay = TimesOfDay.Evening;
+
+            // Initialize our persons. They will share the same game time and time of day
+            Func<DateTime?> gameTimeFunc = () => _dateTime;
+            Func<TimesOfDay> timeOfDayFunc = () => _timeOfDay;
+
+            _person1 = InitialzeNewPerson("Trepliev", _weatherForTrepliev, gameTimeFunc, timeOfDayFunc);
+            _person2 = InitialzeNewPerson("Freud", _weatherForFreud, gameTimeFunc, timeOfDayFunc);
 
             Console.WriteLine("Zara is running");
+
+            // Start the "game loop"
+            _loop = new System.Threading.Thread(LoopThread, 0);
+            _loop.Start();
+        }
+
+        private static Person InitialzeNewPerson(string name, WeatherDescription weather, Func<DateTime?> gameTimeFunc, Func<TimesOfDay> timeOfDayFunc){
+            var p = new Person(name);
+
+            p.Initialize(weather, gameTimeFunc, timeOfDayFunc);
+
+            return p;
+        }
+
+        // Our "game loop"
+        private static void LoopThread(object state)
+        {
+            var time = DateTime.Now;
+            var deltaTime = 0f;
+
+            // Update warmth levels right away, so on a first "frame" we already have one calculated
+            _person1.Body.UpdateWarmthLevelCacheImmediately();
+            _person2.Body.UpdateWarmthLevelCacheImmediately();
+
+            while (true)
+            {
+                // Calculate our delta time
+                deltaTime = (float)(DateTime.Now - time).TotalSeconds;
+
+                // Update our persons
+                _person1.Update(deltaTime);
+                _person2.Update(deltaTime);
+
+                // Remember the new time
+                time = DateTime.Now;
+
+                // Progress our in-game time. We'll not change time of day to make everything simplier
+                _dateTimeCounter += deltaTime;
+
+                if (_dateTimeCounter > 0.05f)
+                {
+                    _dateTime = _dateTime.AddSeconds(0.5d); // in-game time is 10x the real one
+                    _dateTimeCounter = 0f;
+                }
+
+                // Cap the "framerate"
+                System.Threading.Thread.Sleep(33);
+            }
         }
     }
 }
